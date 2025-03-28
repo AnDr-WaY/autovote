02026f98315b47d92313e6306acff512e86ce6b7
AnDr-WaY <andriichampion2008@gmail.com>
2025-03-28 22:19:42 +0200

    1

--- test/test1.py
+++ test/test1.py
@@ -1,24 +1,16 @@
 import undetected_chromedriver as uc
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-import logging 
+import logging
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-options = uc.ChromeOptions()
-options.headless = True  # ОБЯЗАТЕЛЬНО True для хостинга! 
-options.page_load_strategy = 'eager'
-# Дополнительные опции для работы в Linux/Docker на хостинге:
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
-options.add_argument('--window-size=1920x1080') # Иногда помогает
-
-driver = uc.Chrome(options=options, driver_executable_path=uc.find_chrome_executable())
-logger.info("Драйвер запущен.")
-
-logger.info("Получаю страницу loliland.ru (eager strategy)")
-driver.get("https://loliland.ru/")
-driver.quit()
+try:
+    options = uc.ChromeOptions()
+    options.headless = True
+    driver = uc.Chrome(options=options)
+    logger.info("Драйвер запущен успешно!")
+    driver.get("https://www.google.com")
+    logger.info(f"Заголовок страницы: {driver.title}")
+    driver.quit()
+except Exception as e:
+    logger.error(f"Ошибка при запуске драйвера: {e}")
